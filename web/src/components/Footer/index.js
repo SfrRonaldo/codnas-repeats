cad8a2a2db5c21ea44b4a5b8488ccb044816e237
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2021-07-30 14:08:09 -0500

    footer added

---
+++ web/src/components/Footer/index.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <div className="bottom-0 min-w-full absolute">
+      <footer className="bg-primary-dark py-6">
+        <div className="px-4 mx-auto">
+          <div className="max-w-full px-1 mx-auto lg:px-4">
+            <div className="flex text-center justify-center items-center">
+              <p className="text-white font-medium block text-sm sm:text-base">
+                Copyright © 2021. Made by Ronaldo Romario Tunque Cahui. All rights reserved.
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -3,15 +3,20 @@ import RouteWithLoader from '../components/RouteWithLoader'
 import { Routes } from '../routes'
 // pages
 import Home from './Home'
+// components
+import Footer from '../components/Footer'
+import ScrollToTop from '../components/ScrollToTop'
 
 const App = () => {
   return (
     <Router>
-      <div className="font-montserrat">
+      <div className="font-montserrat min-h-screen relative">
+        <ScrollToTop />
         <Switch>
           <RouteWithLoader exact path={Routes.Home.path} component={Home} />
           <Redirect exact path="/" to={Routes.Home.path} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   )
