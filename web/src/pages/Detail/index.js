01f384caa197034cf20dc040f5aae1ba07f4535d
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2021-08-03 06:57:17 -0500

    minor fix

--- web/src/pages/Detail/index.js
+++ web/src/pages/Detail/index.js
@@ -1,4 +1,5 @@
 import { Fragment, useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 import { useParams } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { getRepeatDetailsAction } from '../../actions/repeatActions'
@@ -7,7 +8,7 @@ import General from './General'
 import Structural from './Structural'
 import Conformer from './Conformer'
 
-const Detail = () => {
+const Detail = ({ history }) => {
   const dispatch = useDispatch()
   const params = useParams()
 
@@ -24,22 +25,36 @@ const Detail = () => {
   const general = useSelector((state) => state.repeat.general)
   const structural = useSelector((state) => state.repeat.structural)
   const conformers = useSelector((state) => state.repeat.conformers)
+  const error = useSelector((state) => state.repeat.error)
 
   return (
     <Fragment>
       <div className="pt-6 pb-40">
         <div className="px-4 sm:px-16 lg:px-32 xl:px-48 2xl:px-60 mx-auto">
           <div className="max-w-full px-1 mx-auto lg:px-4">
-            {!general && (
-              <div className="pt-12 space-y-4">
-                <div id="loader" style={{ textAlign: '-webkit-center' }}>
-                  <ReactLoading type="spin" color="#d0646c" />
+            {!general &&
+              (!error ? (
+                <div className="pt-12 space-y-4">
+                  <div id="loader" style={{ textAlign: '-webkit-center' }}>
+                    <ReactLoading type="spin" color="#d0646c" />
+                  </div>
+                  <div className="text-center">
+                    <span>Loading...</span>
+                  </div>
                 </div>
-                <div className="text-center">
-                  <span>Loading...</span>
+              ) : (
+                <div className="py-5 sm:py-10 space-y-4">
+                  <h1 className="text-gray-700 text-3xl md:text-4xl font-bold text-left">Results Not Found</h1>
+                  <h2>The results you are looking for are not found.</h2>
+                  <h2>Try searching again or use the Go Back button below.</h2>
+                  <button
+                    className="px-4 py-2 bg-primary-dark rounded hover:bg-opacity-90 text-white border-1"
+                    onClick={() => history.push('/home')}
+                  >
+                    Go back home
+                  </button>
                 </div>
-              </div>
-            )}
+              ))}
             <div className="pt-8">{general && <General id={pdbId} general={general} />}</div>
             <div className="pt-12">{general && <Structural structural={structural} />}</div>
             <div className="pt-12">{general && <Conformer id={pdbId} general={general} conformers={conformers} />}</div>
@@ -48,6 +63,10 @@ const Detail = () => {
       </div>
     </Fragment>
   )
+}
+
+Detail.propTypes = {
+  history: PropTypes.any.isRequired,
 }
 
 export default Detail
